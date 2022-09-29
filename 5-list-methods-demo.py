084bbbc908663315ca8b710195f6fa035ea764c5
Burak Üstün <79836033+burakbfm@users.noreply.github.com>
2022-09-29 18:31:53 +0300

    ÖDEV 5

---
+++ 5-list-methods-demo.py
@@ -0,0 +1,41 @@
+isimler = ['Ada','Yiğit','Hasan','Beril']
+yaslar = [1998, 2000, 1998, 1987]
+
+# 1-  "Cenk" ismini listenin sonuna ekleyiniz.
+
+
+# 2-  "Sena" değerini listenin başına ekleyiniz.
+
+
+# 3-  "Yiğit" ismini listeden siliniz.
+
+
+# 4-  "Yiğit" isminin indeksi nedir ?
+
+
+# 5-  "Beril" listenin bir elemanı mıdır ?
+
+
+# 6-  Liste elemanlarını ters çevirin.
+
+
+# 7-  Liste elemanlarını alfabetik olarak sıralayınız.
+
+
+# 8-  yaslar listesini rakamsal büyüklüğe göre sıralayınız.
+
+
+# 9-  s = "IPhone X,IPhone XR" karakter dizisini listeye çeviriniz.
+
+
+# 10- yaslar dizisinin en büyük ve en küçük elemanı nedir ?
+
+# 11- yaslar dizisinde kaç tane 1998 değeri vardır ?
+
+
+# 12- yaslar dizisinin tüm elemanlarını siliniz.
+
+
+# 13- Kullanıcıdan alacağınız 3 tane marka bilgisini bir listede saklayınız.
+
+
